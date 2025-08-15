4437772c5e58d9071942522315c46b9348d17bce
Nidhi Y <nidhiyogeesha12@gmail.com>
2025-08-15 18:25:22 +0530

    Add project metadata dictionary

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+project_metadata = {
+    "author": "Nidhi",
+    "version": "1.0",
+    "features": [
+        "CSV data analysis",
+        "Word frequency counter",
+        "Keyword search"
+    ]
+}
